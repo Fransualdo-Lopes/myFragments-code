14af08a5b10e193b4d163d952f389093425c2ff7
Fransualdo Lopes <fransualdo.14@gmail.com>
2024-03-17 15:56:05 -0300

    add function bmi

--- script.js
+++ script.js
@@ -53,3 +53,56 @@ alert( "Hello, " + capitalisedName);
 // idade do cachorro em anos humanos
 var dogAge = prompt(" How old is your dog?");
 var humanAge = ((dogAge - 2) * 4) + 21;
+alert("your dog is " +  humanAge + "years old in human years.");
+
+// linhas sobre funções:
+function getMilk() {   // pegar o leite
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+
+  // Calculando idade tempo de vida restante até 90 anos:
+
+  function lifeInWeeks(age) {
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;        
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+}
+
+lifeInWeeks(25)
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+function bmiCalculator (peso,altura){
+    var bmi = peso/(altura*altura);
+    return bmi;
+}
+
+bmiCalculator(65,1.8);
+console.log("o imc é: ", bmi);
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
