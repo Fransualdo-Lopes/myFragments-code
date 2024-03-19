68673c23d3e88603109cc5e203d38e80bce0b881
Fransualdo Lopes <fransualdo.14@gmail.com>
2024-03-19 10:47:04 -0300

    Leap year or Not leap year.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ alert("you have  written" + tweetCount + " caracters, you have" + (140 - tweetCo
 
 // podemos melhorar o codigo com estas linhas:
 var tweet = prompt("Compose your tweet:");
-var tweetUnder140 = tweet.slice(0,140);
+var tweetUnder140 = tweet.slice(0, 140);
 alert(tweetUnder140);
 //alert("you have  written" + tweetCount + " caracters, you have" + (140 - tweetCount) + "caracters remaining.");
 
@@ -29,7 +29,7 @@ var userName = prompt("what is your name?");
 
 //2 Capitalise the first letter of their name.
 //a isolate the first char.
-var fistChar = userName.slice(0,1);
+var fistChar = userName.slice(0, 1);
 // retorno será a primeira letra do nome
 
 //b Turn the first char up case.
@@ -37,75 +37,101 @@ var upperCaseFirstChar = fistChar.toUpperCase();
 // retorno será a primira letra do nome em caixa alta
 
 //c Isolate the rest of the name.
-var rest0fName = userName.slice(1,userName.length);
+var rest0fName = userName.slice(1, userName.length);
 // retorno será o restante do primeiro nome
 
 //d change the rest of name to lower case 
 var rest0fName = rest0fName.toLowerCase();
 //e concatenate the first char with the rest of the char.
-var capitalisedName = upperCaseFirstChar + rest0fName; 
+var capitalisedName = upperCaseFirstChar + rest0fName;
 
 //3 We use the capitalised version of their name to geet them using an alert.
-alert( "Hello, " + capitalisedName);
+alert("Hello, " + capitalisedName);
 
 //Hello, Fransualdo
 
 // idade do cachorro em anos humanos
 var dogAge = prompt(" How old is your dog?");
 var humanAge = ((dogAge - 2) * 4) + 21;
-alert("your dog is " +  humanAge + "years old in human years.");
+alert("your dog is " + humanAge + "years old in human years.");
 
 // linhas sobre funções:
 function getMilk() {   // pegar o leite
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-  }
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
 
-  // Calculando idade tempo de vida restante até 90 anos:
+// Calculando idade tempo de vida restante até 90 anos:
 
-  function lifeInWeeks(age) {
-    var yearsRemaining = 90 - age;
-    var days = yearsRemaining * 365;        
-    var weeks = yearsRemaining * 52;
-    var months = yearsRemaining * 12;
-    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+function lifeInWeeks(age) {
+  var yearsRemaining = 90 - age;
+  var days = yearsRemaining * 365;
+  var weeks = yearsRemaining * 52;
+  var months = yearsRemaining * 12;
+  console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
 }
 
 lifeInWeeks(25)
 
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
-function bmiCalculator (peso,altura){
-   // var bmi = peso/(altura*altura);
-   var bmi = peso/Math.pow(altura,2)// elevando altura ao quadrado usando "Math.pow"
-    return Math.round(bmi); // Math.round arredonda o numero para o inteiro mais proximo.
-    //return Math.floor(bmi); // Math.floor arredonda um número para o menor inteiro mais próximo
-    //return bmi;
+function bmiCalculator(peso, altura) {
+  // var bmi = peso/(altura*altura);
+  var bmi = peso / Math.pow(altura, 2)// elevando altura ao quadrado usando "Math.pow"
+  return Math.round(bmi); // Math.round arredonda o numero para o inteiro mais proximo.
+  //return Math.floor(bmi); // Math.floor arredonda um número para o menor inteiro mais próximo
+  //return bmi;
 }
 
-bmiCalculator(65,1.8);
-console.log("o imc é: ", bmi);
+bmiCalculator(65, 1.8);
+if (bmi <= 18.5) {
+  console.log("Seu IMC é ", bmi, " então você está abaixo do peso.:");
+}
 
-/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+if (bmi > 18.5) && (bmi <= 24.9){
+  console.log("Seu IMC é ", bmi, " então você tem um peso normal.:");
+}else {
+  console.log("Seu IMC é ", bmi, " então você está acima do peso.:");
+}
 
-var bmi = bmiCalculator(65, 1.8); 
 
-bmi should equal 20 when it's rounded to the nearest whole number.
+
+/* Calculadora de IMC avançada (IF/ELSE)
+Anteriormente, criamos uma função capaz de calcular o IMC. Mas assim que obtivermos o resultado, desejaremos dizer ao usuário o que o número significa.
+
+Escreva uma função que produza (retorne) uma mensagem diferente dependendo do IMC.
+
+
+IMC <18,5, o resultado deve ser: "Seu IMC é <bmi>, então você está abaixo do peso."
+
+IMC 18,5-24,9, a saída deve ser "Seu IMC é <bmi>, então você tem um peso normal."
+
+IMC >24,9, o resultado deverá ser "Seu IMC é <bmi>, então você está acima do peso".
+
+
+
+A mensagem DEVE ser retornada como saída da sua função. Você NÃO PRECISA usar alert, prompt ou console.log neste desafio.
+
+IMPORTANTE o texto da mensagem deve corresponder exatamente para que o código passe na validação. Incluindo pontuação e letras maiúsculas.
 
 */
+
+// utilizando numeros aleatórios Math.randon()
+var n = Math.random();
+console.log(n);
