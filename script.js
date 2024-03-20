38aa570dbeb909ce4e790a888cda16af50b74f3a
Fransualdo Lopes <fransualdo.14@gmail.com>
2024-03-20 09:15:01 -0300

    FizzBuzz até 100 usando while.

--- script.js
+++ script.js
@@ -165,3 +165,24 @@ function whosPaying(names) {
 
 var names = ['Maria', 'João', 'Pedro', 'Tiago'];
 console.log(whosPaying(names));
+
+//FizzBuzz até 100 usando while:
+function fizzBuzz() {
+  var output = [];
+  var count = 1;
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
+  }
+  return output;
+}
+
+console.log(fizzBuzz());
